74bb77af14a631b26129f5b0b25865e43b10bd39
Fatjon Dauti <fdauti@myseneca.ca>
2021-04-04 17:13:55 -0400

    Completing all functions.

--- a2s_fdauti.py
+++ a2s_fdauti.py
@@ -11,23 +11,37 @@ or anything except for submission for grading.
 I understand that the Academic Honesty Policy will be enforced and violators 
 will be reported and appropriate action will be taken.
 '''
-
 env.user = "student"
 env.port = '7904'
 env.hosts = ['myvmlab.senecacollege.ca']
 
 def addUser(name):
     '''add a user with given user name to remote system'''
-    return
- 
-def listUser():
-    '''return a list of shell user on a remote system'''
-    return
-
-def listSysUser():
-    '''return a list of system (non-shell) user'''
-    return
+    cmd = "grep ^"+name+" /etc/passwd"
+    result = run(cmd, warn_only=True, quiet=True)
+    if result.failed:
+        sudo("useradd -m "+name)
+        print("User '"+name+"' added!")
+    else:
+        print("User '"+name+"' already exists!")
 
 def findUser(name):
     '''find user with a given user name'''
-    return
+    cmd = "grep ^"+name+" /etc/passwd"
+    result = run(cmd, warn_only=True, quiet=True)
+    if result.succeeded:
+        print("Found user '"+name+"' on the system!")
+    else:
+        print("User '"+name+"' NOT found!")
+
+def listUser():
+    '''return a list of shell user on a remote system'''
+    cmd = "grep /bin/bash$ /etc/passwd | cut -d: -f1"
+    result = run(cmd)
+    print(result.split())
+
+def listSysUser():
+    '''return a list of system (non-shell) user'''
+    cmd = "grep -v /bin/bash$ /etc/passwd | cut -d: -f1"
+    result = run(cmd)
+    print(result.split())
