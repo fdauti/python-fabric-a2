7e0d6a5d664c9689b8221604f8ec57f111fea920
Fatjon Dauti <fdauti@myseneca.ca>
2021-04-07 17:19:12 -0400

    commands used for testing

---
+++ test.cmd
@@ -0,0 +1,8 @@
+ fab -f a2s_fdauti.py findUser:test2 &>> a2s_fdauti.txt 
+ fab -f a2s_fdauti.py findUser:test &>> a2s_fdauti.txt 
+ fab -f a2s_fdauti.py addUser:test2 &> a2s_fdauti.txt 
+ fab -f a2s_fdauti.py addUser:test2 &>> a2s_fdauti.txt 
+ fab -f a2s_fdauti.py findUser:test2 &>> a2s_fdauti.txt 
+ fab -f a2s_fdauti.py findUser:test &>> a2s_fdauti.txt 
+ fab -f a2s_fdauti.py listUser &>> a2s_fdauti.txt
+ fab -f a2s_fdauti.py listSysUser &>> a2s_fdauti.txt 
