a59c49a3c37a1f63981b43142b606b52b647f822
Fatjon Dauti <31435628+fdauti@users.noreply.github.com>
2021-04-15 22:40:56 -0400

    Update test.cmd

--- test.cmd
+++ test.cmd
@@ -1,5 +1,3 @@
- fab -f a2s_fdauti.py findUser:test2 &>> a2s_fdauti.txt 
- fab -f a2s_fdauti.py findUser:test &>> a2s_fdauti.txt 
  fab -f a2s_fdauti.py addUser:test2 &> a2s_fdauti.txt 
  fab -f a2s_fdauti.py addUser:test2 &>> a2s_fdauti.txt 
  fab -f a2s_fdauti.py findUser:test2 &>> a2s_fdauti.txt 
